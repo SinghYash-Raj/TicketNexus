d968f0f2647906c4c5dd009a19102ef3965c0429
YashRajSingh <yash13raj.singh@gmail.com>
2024-06-29 02:10:38 +0530

    Order Events Publisher

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,7 +5,8 @@ import {
   NotAuthorizedError,
 } from '@yrsticket/common';
 import { Order, OrderStatus } from '../models/order';
-
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 const router = express.Router();
 
 router.delete(
@@ -13,8 +14,10 @@ router.delete(
   requireAuth,
   async (req: Request, res: Response) => {
     const { orderId } = req.params;
-
-    const order = await Order.findById(orderId);
+  
+    //The code snippet retrieves an order by its ID and includes the associated ticket details within the retrieved order document.
+    //This is useful when you want to work with both the order and its related ticket data in a single query.
+    const order = await Order.findById(orderId).populate('ticket');
 
     if (!order) {
       throw new NotFoundError();
@@ -26,6 +29,12 @@ router.delete(
     await order.save();
 
     // publishing an event saying this was cancelled!
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
 
     res.status(204).send(order);
   }
