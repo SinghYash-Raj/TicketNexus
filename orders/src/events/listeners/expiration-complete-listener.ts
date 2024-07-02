ed5b7d4d2891deff6fffeecabc72cad6ab855abb
YashRajSingh <yash13raj.singh@gmail.com>
2024-07-03 00:12:50 +0530

    Implemented Payment-Service

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -19,6 +19,10 @@ import {
       if (!order) {
         throw new Error('Order not found');
       }
+
+      if (order.status === OrderStatus.Complete) {
+        return msg.ack();
+      }
   
       order.set({
         status: OrderStatus.Cancelled,
